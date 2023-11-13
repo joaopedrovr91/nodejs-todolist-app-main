0483f5b3e2d30fee7fbea6dea3cfc7dd1b437fa5
joaopedrovr91 <joaopedrovr91@gmail.com>
2023-11-13 11:21:02 -0300

    finish CRUD

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 3000;  
+const appCustom = require("./config/Appcustom");
+
+appCustom(app, express);
+
+app.listen(port, (error) => {
+    if(error) {
+        console.log("Deu erro");
+        return;
+    }
+    console.log("Subiu show asd")
+}); 
---
+++ infraestrutura/conexao.js
@@ -0,0 +1,11 @@
+const mysql = require("mysql2");
+
+const conexao = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "1q2w3e4r",
+    database: "controle_atendimento",
+})
+
+module.exports = conexao;
